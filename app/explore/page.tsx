5c13a1e025701afb62c48923960b6b92dc4f3956
os1907 <osamatrezey@gmail.com>
2024-08-10 22:12:48 +0300

    fix

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+'use client'
+import React, { useEffect, useState } from 'react';
 import { genre } from '../Utilities/apis';
 import Link from 'next/link';
 import { Igenre } from '../Utilities/Interface/interfaces';
 
 
+interface Igen{
+    id: number ,
+        name: string
+}
+interface Result {
+    genres:Igen[]
+}
 
-export default async function ExploreParent(props: Igenre) {
-    const media = await genre(`${props.type === "Movies" ? "movie" : "tv"}`);
+export default  function ExploreParent(props: Igenre) {
+    const [media, setmedia] = useState<Result>();
+    const fetchGenre =  async() => {
+       const data = await genre(`${props.type === "Movies" ? "movie" : "tv"}`);
+       setmedia(data)
+    }
+    useEffect(() => {
+        fetchGenre()
+    },[])
 
     return (
         <>
@@ -14,7 +29,7 @@ export default async function ExploreParent(props: Igenre) {
                 <span className='text-4xl lg:text-6xl'>|</span> Choose your type
             </h3>
             <div className='relative z-50 flex mx-4 lg:mx-24 items-center gap-x-2 overflow-scroll scrollbar-hide'>
-                {media?.genres.map((item: { id: number, name: string }) => (
+                {media?.genres?.map((item:Igen) => (
                     <div key={item.id} className='bg-white borderGlass rounded-full flex items-center px-4 lg:px-10 py-1 min-w-max'>
                         <Link href={`explore/${item?.id}-${item?.name?.replace(/ /g, '-')}-${props.type === "Movies" ? "movie" : "tv"}`}>
                             <p className='text-black text-center text-sm lg:text-lg'>
